3b5da6887410aba149e242e19bc83a2a9a565b81
Natalie Schoch <natalie@zenpayroll.com>
2016-11-23 09:17:01 -0800

    removed old scss

---
+++ gusto.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<head>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css" TYPE="text/css">
+  <link rel="icon" type="image/png" href="images/favicon.ico"/>
+  <title>Natalie Schoch - Designer</title>
+  <meta name="keywords" content="natalie, schoch, designer, graphic, illustration, infographics">
+  <meta name="description" content="Natalie Schoch - Portfolio">
+  <meta name="author" content="Natalie Schoch">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body id='portfolio'>
+  <header>
+    <div class='container'>        
+      <div class='column'>
+        <h1>Natalie Schoch</h1>
+      </div>
+    </div>
+  </header>
+  <section id='main'>
+    <div class='container'>
+      <h1>hi</h1>
+      <h2>lorum ipsum</h2>
+    </div>
+  </section>
+  <footer>
+    <div class='container'>
+      <div class='bio'>
+        <p>Natalie is an interaction designer working and living in San Francisco, CA. She specializes in design and front-end development.</p>
+        <ul class='footer-links'>
+          <li><a href='#'>Dribbble</a></li>
+          <li><a href='#'>Twitter</a></li>
+          <li><a href='#'>Github</a></li>
+        </ul>
+      </div>
+    </div>
+    <script src="https://use.typekit.net/uxj0kgs.js"></script>
+    <script>try{Typekit.load({ async: true });}catch(e){}</script>
+  </footer>
+</body>
