224ba81c2991e62e26e19071274867cb0ff16295
Natalie Schoch <natalie@zenpayroll.com>
2016-11-23 09:33:29 -0800

    added header file

--- gusto.php
+++ gusto.php
@@ -1,30 +1,6 @@
-
-<!DOCTYPE html>
-<head>
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="css/style.css" TYPE="text/css">
-  <link rel="icon" type="image/png" href="images/favicon.ico"/>
-  <title>Natalie Schoch - Designer</title>
-  <meta name="keywords" content="natalie, schoch, designer, graphic, illustration, infographics">
-  <meta name="description" content="Natalie Schoch - Portfolio">
-  <meta name="author" content="Natalie Schoch">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
+<?php include 'header.php';?>
 
 <body id='portfolio'>
-  <header>
-    <div class='container'>        
-      <div class='column'>
-        <h1>Natalie Schoch</h1>
-      </div>
-    </div>
-  </header>
-  <section id='main'>
-    <div class='container'>
-      <h1>hi</h1>
-      <h2>lorum ipsum</h2>
-    </div>
-  </section>
   <footer>
     <div class='container'>
       <div class='bio'>
---
+++ header.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<head>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css" TYPE="text/css">
+  <link rel="icon" type="image/png" href="images/favicon.ico"/>
+  <title>Natalie Schoch - Designer</title>
+  <meta name="keywords" content="natalie, schoch, designer, graphic, illustration, infographics">
+  <meta name="description" content="Natalie Schoch - Portfolio">
+  <meta name="author" content="Natalie Schoch">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+ <header>
+    <div class='container'>
+      <div class='column'>
+        <ul class='nav'>
+          <li><a href='#'>Work</a></li>
+          <li><a href='#'>About</a></li>
+          <li><a href='#'>Contact</a></li>
+        </ul>
+      </div>
+      <div class='column'>
+        <h1>Natalie Schoch</h1>
+      </div>
+    </div>
+  </header>
